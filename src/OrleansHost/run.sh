323cbc5922e23660d0f7b6a76beed710f7628dd6
jthelin <github@thelin.me>
2015-03-08 21:27:30 -0700

    Run-Silo Scripts
    - Normalize the "Run Silo" script name.
    
    - Add "Run Silo" script run.sh for *nix.
    
    - Set execute permission on run.sh
    
    - Updated way that unix scripts find the script directory they are running from, based on this highly rated answer on Stack Overflow.
    http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in/246128#246128
    
    - Add command 'bang' header to unit scripts so they always run under bash to avoid any potential incompatilitities if main user shell is different [eg ksh, zsh, etc.] (+1 squashed commits)
    
    - Include default OrleansConfiguration.xml into built Binaries directory to allow smoke test silo to run in place.
    
    - Added build.sh script to download NuGet.exe with wget then call xbuild.
    - For some unknown reason the NuGet auto-download bootstrap function does not work when running in xbuild.

---
+++ src/OrleansHost/run.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+siloName="MonoSilo"
+##if [ -e "$1" ]
+##then
+##  siloName=$1
+##  shift
+##fi
+
+cd $DIR
+
+echo SiloName= $siloName
+echo Starting Orleans node in `pwd`
+mono OrleansHost.exe $siloName $*
