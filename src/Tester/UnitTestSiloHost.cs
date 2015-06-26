f3ea488102db77ba4c0c3eb92ac37da5b4fde6a3
Sergey Bykov <sbykov@microsoft.com>
2015-06-25 19:18:29 -0700

    Migration of liveness tests.

--- src/Tester/UnitTestSiloHost.cs
+++ src/Tester/UnitTestSiloHost.cs
@@ -48,5 +48,25 @@ namespace UnitTests.Tester
             : base(siloOptions, clientOptions)
         {
         }
+
+        protected static string DumpTestContext(TestContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat(@"TestName={0}", context.TestName).AppendLine();
+            sb.AppendFormat(@"FullyQualifiedTestClassName={0}", context.FullyQualifiedTestClassName).AppendLine();
+            sb.AppendFormat(@"CurrentTestOutcome={0}", context.CurrentTestOutcome).AppendLine();
+            sb.AppendFormat(@"DeploymentDirectory={0}", context.DeploymentDirectory).AppendLine();
+            sb.AppendFormat(@"TestRunDirectory={0}", context.TestRunDirectory).AppendLine();
+            sb.AppendFormat(@"TestResultsDirectory={0}", context.TestResultsDirectory).AppendLine();
+            sb.AppendFormat(@"ResultsDirectory={0}", context.ResultsDirectory).AppendLine();
+            sb.AppendFormat(@"TestRunResultsDirectory={0}", context.TestRunResultsDirectory).AppendLine();
+            sb.AppendFormat(@"Properties=[ ");
+            foreach (var key in context.Properties.Keys)
+            {
+                sb.AppendFormat(@"{0}={1} ", key, context.Properties[key]);
+            }
+            sb.AppendFormat(@" ]").AppendLine();
+            return sb.ToString();
+        }
     }
 }
